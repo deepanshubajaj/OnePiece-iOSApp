32272eff2c8bb954608a022c0ed8e8c3fc4aa7a8
deepanshubajaj <deepanshu.bajaj98@gmail.com>
2025-05-21 18:22:20 +0530

    OnePiece iOS App Code Files

---
+++ OnePiece/main.py
@@ -0,0 +1,14 @@
+import json
+
+path = "/Users/maniklakhanpal/OnePiece/OnePiece/Arcs.json"
+
+with open(path, 'r') as file:
+    content = json.load(file)
+    for i in content:
+        straw_hats = i["straw_hats"]
+        print(straw_hats)
+        lowercase_straw_hats = [hat.lower() for hat in straw_hats]
+        i["straw_hats"] = lowercase_straw_hats
+
+with open("/Users/maniklakhanpal/OnePiece/OnePiece/Arcs.json", 'w') as file:
+    json.dump(content, file)
---
+++ OnePiece/tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+with open(path, 'w') as file:
+    json.dump(content, file)
